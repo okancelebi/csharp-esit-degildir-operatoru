3d2cb833595740a35525d854279b7a0b6c39943d
Okan Çelebi <okancelebi06@gmail.com>
2023-02-07 20:57:45 +0300

    Add files via upload

---
+++ csharp eşit değildir öperatörü/csharp eşit değildir öperatörü/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_eşit_değildir_öperatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eşit değildir operatörü = !=
+            int sayi1 = 5;
+            int sayi2 = 5;
+            bool kontrolet = sayi1 != sayi2;
+            Console.WriteLine(kontrolet);
+            Console.ReadLine();
+        }
+    }
+}
